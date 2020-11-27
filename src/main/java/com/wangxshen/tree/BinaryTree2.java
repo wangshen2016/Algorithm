efac7bb037457909cd75467926e23c5deda5d0c7
wangshen2016 <doer_wangshen@163.com>
2020-11-27 18:33:59 +0800

    add：求中序遍历某节点的前驱节点

--- src/main/java/com/wangxshen/tree/BinaryTree2.java
+++ src/main/java/com/wangxshen/tree/BinaryTree2.java
@@ -38,6 +38,33 @@ public class BinaryTree2 {
         }
     }
 
+    //获取中序遍历某节点的前驱节点
+    public static TreeNode getPrecursor(TreeNode head) {
+        if (head == null) {
+            return null;
+        }
+        //如果有左子树，则其后继节点就是左孩子的最右节点
+        if (head.lchild != null) {
+            head = head.lchild;
+            while (head.rchild != null) {
+                head = head.rchild;
+            }
+            return head;
+        } else {
+            // 1. 由该节点向上看，如果该节点是右孩子，那么其后继就是其父节点，
+            // 2. 如果该节点x是左孩子，那么就一直向上看，
+            // 直到某个节点a的右孩子是该节点的某个父系节点b时（b = x.parent...parent）,
+            // 节点a就是x的后继节点
+            TreeNode parent = head.parent;
+
+            while (parent != null && parent.rchild != head) {
+                head = parent;
+                parent = head.parent;
+            }
+            return parent;
+        }
+    }
+
     public static void main(String[] args) {
         Integer[] source = new Integer[] {8,5,2,null,null,7,1,null,null,null,6,null,4,0,null,null,3,null,null};
         TreeNode head = TreeNode.build(source);
@@ -47,7 +74,10 @@ public class BinaryTree2 {
         while (!nodes.isEmpty()) {
             TreeNode poll = nodes.poll();
             TreeNode inSuccessor = getInSuccessor(poll);
+            TreeNode inPredecessor = getPrecursor(poll);
+            System.out.println("value: " + poll.value + " precursor: " + (inPredecessor == null ? "--" : inPredecessor.value));
             System.out.println("value: " + poll.value + " successor: " + (inSuccessor == null ? "--" : inSuccessor.value));
+            System.out.println();
         }
     }
 }
